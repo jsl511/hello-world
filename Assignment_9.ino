012a18edbf1abdf48a244eedec720a6e8d0f59a1
jsl511 <71199958+jsl511@users.noreply.github.com>
2021-07-14 10:44:09 -0400

    Add files via upload

---
+++ Assignment_9.ino
@@ -0,0 +1,17 @@
+void setup() {
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+void loop() {
+  int val=digitalRead(2);
+  if(val==HIGH)
+  {
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1000/16);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000/16);                       
+  }
+
+}
